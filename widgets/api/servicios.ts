656cb91a155c9c48e81a8b87479912fdbc1ac0a8
thiagoriost <thiagoriostapia@gmail.com>
2024-12-09 07:56:29 -0500

    version para produccion 09122024

--- widgets/api/servicios.ts
+++ widgets/api/servicios.ts
@@ -22,9 +22,31 @@ const urls = {
     v_predios_conflicto_mun: `${MapServerMunicipal}/11`,
     v_predios_ley2da_mun: `${MapServerMunicipal}/12`,
     v_predios_etnicos_por_mun: `${MapServerMunicipal}/13`,
-    v_predios_etnicos_mun: `${MapServerMunicipal}/14`
+    v_predios_etnicos_mun: `${MapServerMunicipal}/14`,
+    v_predios_zrc_mun: `${MapServerMunicipal}/15`
+
   },
   indicadoresNaci: { // nacionales Indicadores_nacionales_municipales
+    v_predios_campesinos_orip_mun: `${MapServerMunicipal}/3`,
+    v_predios_campesinos_adj_mun: `${MapServerMunicipal}/4`,
+    v_predios_sub_integrales_mun: `${MapServerMunicipal}/5`,
+    v_predios_campesinos_tit_mun: `${MapServerMunicipal}/6`,
+    v_predios_titulados_muj_mun: `${MapServerMunicipal}/7`,
+    v_predios_restierras_mun: `${MapServerMunicipal}/8`,
+    v_indice_gini_ids_mun: `${MapServerMunicipal}/9`,
+    v_predios_uaf_mun: `${MapServerMunicipal}/10`,
+    v_predios_conflicto_nacmun: `${mapServerNal}/10`,
+    v_predios_conflicto_mun: `${mapServerNal}/11`,
+    v_predios_etnicos_por_nacmun: `${mapServerNal}/11`,
+    v_predios_ley2da_mun: `${MapServerMunicipal}/12`,
+    v_predios_etnicos_nacmun: `${mapServerNal}/12`,
+    v_predios_etnicos_por_mun: `${MapServerMunicipal}/13`,
+    v_predios_etnicos_mun: `${MapServerMunicipal}/14`,
+    v_indice_gini_ids_nac_tot: `${mapServerNal}/17`,
+    v_predios_ley2da_nacmun: `${mapServerNal}/18`,
+    v_predios_zrc_nacmun: `${mapServerNal}/19`
+  },
+  /* indicadoresNaci: { // nacionales Indicadores_nacionales_municipales
     v_predios_campesinos_orip_nacmun: `${mapServerNal}/3`,
     v_predios_campesinos_adj_nacmun: `${mapServerNal}/4`,
     v_predios_sub_integrales_nacmun: `${mapServerNal}/5`,
@@ -32,16 +54,13 @@ const urls = {
     v_predios_titulados_muj_nacmun: `${mapServerNal}/7`,
     v_predios_restierras_nacmun: `${mapServerNal}/8`,
     v_predios_uaf_nacmun: `${mapServerNal}/9`,
-    v_predios_conflicto_nacmun: `${mapServerNal}/10`,
     v_predios_etnicos_por_nacmun: `${mapServerNal}/11`,
     v_predios_etnicos_nacmun: `${mapServerNal}/12`,
     v_predios_etnicos_porcnac: `${mapServerNal}/13`,
     v_predios_uaf_porcnac: `${mapServerNal}/14`,
     v_predios_zrc_porcnac: `${mapServerNal}/15`,
     v_predios_ley2da_porcnac: `${mapServerNal}/16`,
-    v_indice_gini_ids_nac_tot: `${mapServerNal}/17`,
-    v_predios_ley2da_nacmun: `${mapServerNal}/18`
-  },
+  }, */
   indicadoresDepartal: { // Departamentales Indicadores_departamentos
     v_predios_campesinos_orip_depto: `${mapServerDepartal}/3`,
     v_predios_campesinos_adj_depto: `${mapServerDepartal}/4`,
@@ -57,7 +76,11 @@ const urls = {
     v_predios_etnicos_porcdepto: `${mapServerDepartal}/14`,
     v_predios_uaf_porcdepto: `${mapServerDepartal}/15`,
     v_predios_zrc_porcdepto: `${mapServerDepartal}/16`,
-    v_predios_ley2da_porcdepto: `${mapServerDepartal}/17`
+    v_predios_zrc_depto: `${mapServerDepartal}/18`,
+    v_predios_actualizados_depto: `${mapServerDepartal}/19`,
+    v_municipios_actualizados_depto: `${mapServerDepartal}/20`,
+    v_predios_ley2da_depto: `${mapServerDepartal}/21`
+
   },
   indicadoresNaciAlfanumerica: { // nacionales para graficas estadisticas
     v_predios_fondo_tierras_sumnac: `${mapServerNal}/19`,
